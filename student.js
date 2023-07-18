b9ce9b445a630c97d9d1618aa313c4a81803b062
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2023-07-18 14:40:45 +0600

    Student func access

---
+++ student.js
@@ -0,0 +1,18 @@
+const getName = () =>{
+    return "Sumaia";
+     
+}
+const getAge = () =>{
+    return "24";
+}
+const cgpa=3.92
+
+/*exports.getName=getName;
+exports.getAge=getAge;
+exports.result=cgpa;*/
+module.exports={
+    getName,
+    getAge,
+    cgpa
+}
+
